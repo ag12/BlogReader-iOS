a9777cc79c2e6e29cad65a1fe80a96cd77aa54eb
ag12 <amirg12@hotmail.no>
2014-12-15 18:53:38 +0100

    Initial Commit

---
+++ BlogReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlogReader
+//
+//  Created by Amir Ghoreshi on 15/12/14.
+//  Copyright (c) 2014 AM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlogReader/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  BlogReader
+//
+//  Created by Amir Ghoreshi on 15/12/14.
+//  Copyright (c) 2014 AM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
